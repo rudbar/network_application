967019b3f61b2f6bd30f08f3a776c13920e8950d
Ian Kachan <kachan.yanf@gmail.com>
2020-05-15 00:42:40 +0500

    Started styling the login and registration forms

--- register.php
+++ register.php
@@ -15,75 +15,88 @@ require 'includes/form_handlers/login_handler.php';
 </head>
 <body>
 
-    <form action="register.php" method="POST" accept-charset="UTF-8">
-        <input type="email" name="log_email" placeholder="Email Адрес" value="<?php 
-        if(isset($_SESSION['log_email'])) {
-            echo $_SESSION['log_email'];
-        } 
-        ?>" required>
-        <br>
-        <input type="password" name="log_password" placeholder="Пароль">
-        <br>
-        <input type="submit" name="login_button" value="Войти">
-        <br>
-
-        <?php if(in_array("Неправильный Email или пароль<br>", $error_array)) echo  "Неправильный Email или пароль<br>"; ?>
-
-    </form>
-    
-    <form action="register.php" method="POST" accept-charset="UTF-8">
-        <input type="text" name="reg_fname" placeholder="Имя" value="<?php 
-        if(isset($_SESSION['reg_fname'])) {
-            echo $_SESSION['reg_fname'];
-        } 
-        ?>" required>
-        <br>
-        <?php if(in_array("Ваше имя должно содержать от 2 до 25 символов<br>", $error_array)) echo "Ваше имя должно содержать от 2 до 25 символов<br>"; ?>
+    <div class="wrapper">
         
+        <div class="login_box">
+        
+            <div class="login_header">
+                <h1>Название</h1>
+                Войдите или зарегестрируйтесь ниже!
+            </div>
+
+            <form action="register.php" method="POST" accept-charset="UTF-8">
+                <input type="email" name="log_email" placeholder="Email Адрес" value="<?php 
+                if(isset($_SESSION['log_email'])) {
+                    echo $_SESSION['log_email'];
+                } 
+                ?>" required>
+                <br>
+                <input type="password" name="log_password" placeholder="Пароль">
+                <br>
+                <input type="submit" name="login_button" value="Войти">
+                <br>
+
+                <?php if(in_array("Неправильный Email или пароль<br>", $error_array)) echo  "Неправильный Email или пароль<br>"; ?>
+
+            </form>
+            
+            <form action="register.php" method="POST" accept-charset="UTF-8">
+                <input type="text" name="reg_fname" placeholder="Имя" value="<?php 
+                if(isset($_SESSION['reg_fname'])) {
+                    echo $_SESSION['reg_fname'];
+                } 
+                ?>" required>
+                <br>
+                <?php if(in_array("Ваше имя должно содержать от 2 до 25 символов<br>", $error_array)) echo "Ваше имя должно содержать от 2 до 25 символов<br>"; ?>
+                
 
 
-        <input type="text" name="reg_lname" placeholder="Фамилия" value="<?php 
-        if(isset($_SESSION['reg_lname'])) {
-            echo $_SESSION['reg_lname'];
-        } 
-        ?>" required>
-        <br>
-        <?php if(in_array("Ваша фамилия должна содержать от 2 до 25 символов<br>", $error_array)) echo "Ваша фамилия должна содержать от 2 до 25 символов<br>"; ?>
+                <input type="text" name="reg_lname" placeholder="Фамилия" value="<?php 
+                if(isset($_SESSION['reg_lname'])) {
+                    echo $_SESSION['reg_lname'];
+                } 
+                ?>" required>
+                <br>
+                <?php if(in_array("Ваша фамилия должна содержать от 2 до 25 символов<br>", $error_array)) echo "Ваша фамилия должна содержать от 2 до 25 символов<br>"; ?>
 
 
-        <input type="email" name="reg_email" placeholder="Email" value="<?php 
-        if(isset($_SESSION['reg_email'])) {
-            echo $_SESSION['reg_email'];
-        } 
-        ?>" required>
-        <br>
+                <input type="email" name="reg_email" placeholder="Email" value="<?php 
+                if(isset($_SESSION['reg_email'])) {
+                    echo $_SESSION['reg_email'];
+                } 
+                ?>" required>
+                <br>
 
-        <input type="email" name="reg_email2" placeholder="Подтвердите Email" value="<?php 
-        if(isset($_SESSION['reg_email2'])) {
-            echo $_SESSION['reg_email2'];
-        } 
-        ?>" required>
-        <br>
-        <?php if(in_array("Данный почтовый адрес уже используется<br>", $error_array)) echo "Данный почтовый адрес уже используется<br>";
-        else if(in_array("Неправильный формат почтового адреса<br>", $error_array)) echo "Неправильный формат почтового адреса<br>";
-        else if(in_array("Почтовые адресы не совпадают<br>", $error_array)) echo "Почтовые адресы не совпадают<br>"; ?>
+                <input type="email" name="reg_email2" placeholder="Подтвердите Email" value="<?php 
+                if(isset($_SESSION['reg_email2'])) {
+                    echo $_SESSION['reg_email2'];
+                } 
+                ?>" required>
+                <br>
+                <?php if(in_array("Данный почтовый адрес уже используется<br>", $error_array)) echo "Данный почтовый адрес уже используется<br>";
+                else if(in_array("Неправильный формат почтового адреса<br>", $error_array)) echo "Неправильный формат почтового адреса<br>";
+                else if(in_array("Почтовые адресы не совпадают<br>", $error_array)) echo "Почтовые адресы не совпадают<br>"; ?>
 
-        <input type="password" name="reg_password" placeholder="Пароль" required>
-        <br>
-        <input type="password" name="reg_password2" placeholder="Подтвердите пароль" required>
-        <br>
-        <?php if(in_array("Ваши пароли не совпадают<br>", $error_array)) echo "Ваши пароли не совпадают<br>";
-        else if(in_array("Ваш пароль может содеражть только буквы латинского алфавита или цифцры<br>", $error_array)) echo "Ваш пароль может содеражть только буквы латинского алфавита или цифцры<br>";
-        else if(in_array("Ваш пароль должен содержать от 5 до 30 символов<br>", $error_array)) echo "Ваш пароль должен содержать от 5 до 30 символов<br>"; ?>
-
-
-        <input type="submit" name="register_button" value="Зарегестрироваться">
-        <br>
-
-        <?php if(in_array("<span style='color: #14C800;'>Все готово! Теперь можно войти!</span><br>", $error_array)) echo "<span style='color: #14C800;'>Все готово! Теперь можно войти!</span><br>"; ?>
+                <input type="password" name="reg_password" placeholder="Пароль" required>
+                <br>
+                <input type="password" name="reg_password2" placeholder="Подтвердите пароль" required>
+                <br>
+                <?php if(in_array("Ваши пароли не совпадают<br>", $error_array)) echo "Ваши пароли не совпадают<br>";
+                else if(in_array("Ваш пароль может содеражть только буквы латинского алфавита или цифцры<br>", $error_array)) echo "Ваш пароль может содеражть только буквы латинского алфавита или цифцры<br>";
+                else if(in_array("Ваш пароль должен содержать от 5 до 30 символов<br>", $error_array)) echo "Ваш пароль должен содержать от 5 до 30 символов<br>"; ?>
 
 
-    </form>
+                <input type="submit" name="register_button" value="Зарегестрироваться">
+                <br>
+
+                <?php if(in_array("<span style='color: #14C800;'>Все готово! Теперь можно войти!</span><br>", $error_array)) echo "<span style='color: #14C800;'>Все готово! Теперь можно войти!</span><br>"; ?>
+
+
+            </form>
+
+        </div>
+
+    </div>
 
 
 
