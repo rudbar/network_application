694a4bbf44f43a9f1fe9ce3655cc633ea61d36d3
Ian Kachan <kachan.yanf@gmail.com>
2020-05-18 06:29:42 +0500

    Added the post mechanism

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -44,6 +44,128 @@ class Post {
         }
     }
 
+    public function loadPostsFriends() {
+        $str = ""; //возвращаемая строка
+        $data = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
+
+        while($row = mysqli_fetch_array($data)) {
+            $id = $row['id'];
+            $body = $row['body'];
+            $added_by = $row['added_by'];
+            $date_time = $row['date_added'];
+
+            //для user_to
+            if($row['user_to'] == "none") {
+                $user_to = "";
+            }
+            else {
+                $user_to_obj = new User($con, $row['user_to']);
+                $user_to_name = $user_to_obj->getFirstAndLastName();
+                $user_to = "для <a href='" . $row['user_to'] ."'>" . $user_to_name . "</a>";
+            }
+
+            //Проверка закрыт ли аккаунт пользователя, кто оставил пост
+            $added_by_obj = new User($this->con, $added_by);
+            if($added_by_obj->isClosed()) {
+                continue;
+            }
+
+            $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
+            $user_row = mysqli_fetch_array($user_details_query);
+            $first_name = $user_row['first_name'];
+            $last_name = $user_row['last_name'];
+            $profile_pic = $user_row['profile_pic'];
+
+            //Время
+            $date_time_now = date("Y-m-d H:i:s");
+            $start_date = new DateTime($date_time); //Время поста
+            $end_date = new DateTime($date_time_now); //Текущее время
+            $interval = $start_date->diff($end_date); // Разница между датами
+            if($interval->y >= 1) {
+                if($interval == 1)
+                    $time_message = $interval->y . " год назад"; // 1 год назад
+                else
+                    $time_message = $interval->y . " года назад"; // 1+ года назад
+            }
+            else if ($interval-> m >= 1) {
+                if($interval->d == 0) {
+                    $days = " назад";
+                }
+                else if($interval->d == 1) {
+                    $days = $interval->d . " день назад";
+                }
+                else {
+                    $days = $interval->d . " дней назад";
+                }
+
+
+                if($interval->m == 1) {
+                    $time_message = $interval->m . " месяц". $days;
+                }
+                else {
+                    $time_message = $interval->m . " месяца". $days;
+                }
+            }
+            else if($interval->d >= 1) {
+                if($interval->d == 1) {
+                    $time_message = "Вчера";
+                }
+                else {
+                    $time_message = $interval->d . " дней назад";
+                }
+            }
+            else if($interval->h >= 1) {
+                if($interval->h == 1) {
+                    $time_message = $interval->h . " час назад";
+                }
+                else {
+                    $time_message = $interval->h . " часа назад";
+                }
+            }
+            else if($interval->i >= 1) {
+                if($interval->i == 1) {
+                    $time_message = $interval->i . " минуту назад";
+                }
+                else {
+                    $time_message = $interval->i . " минуты назад";
+                }
+            }
+            else {
+                if($interval->s < 30) {
+                    $time_message = "Только что";
+                }
+                else {
+                    $time_message = $interval->s . " секунд назад";
+                }
+            }
+
+            $str .= "<div class='status_post'>
+                        <div class='post_profile_pic'>
+                            <img src='$profile_pic' width='50px'>
+                        </div>
+
+                        <div class='posted_by' style='color:#ACACAC;'>
+                            <a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+                        </div>
+                        <div id='post_body'>
+                            $body
+                            <br>
+                        </div>
+
+                    </div>";
+
+
+
+        }
+
+        echo $str;
+
+
+    }
+
+
+
+
 
 }
 
