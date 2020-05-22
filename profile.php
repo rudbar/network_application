f370870127f5b0f1431be2316c8d71d41529e9e5
Ian Kachan <kachan.yanf@gmail.com>
2020-05-22 22:44:18 +0500

    Adding functionality

--- profile.php
+++ profile.php
@@ -12,6 +12,19 @@ if(isset($_GET['profile_username'])) {
     $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
 }
 
+if(isset($_POST['remove_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->removeFriend($username);
+}
+
+if(isset($_POST['add_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->sendRequest($username);
+}
+if(isset($_POST['respond_request'])) {
+    header("Location: requests.php");
+}
+
 ?>
 
     <style type="text/css">
@@ -31,7 +44,7 @@ if(isset($_GET['profile_username'])) {
             <p><?php echo "Друзья: " . $num_friends; ?></p>
         </div>
 
-        <form action="<?php echo $username; ?>">
+        <form action="<?php echo $username; ?>" method="POST">
             <?php 
             $profile_user_obj = new User($con, $username); 
             if($profile_user_obj->isClosed()) {
